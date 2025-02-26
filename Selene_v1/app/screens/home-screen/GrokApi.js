b6c3421f1f023dd8f440970d63235f41278f5331
Ajay Cyriac <98336671+indianaranger@users.noreply.github.com>
2025-02-27 00:24:45 +0530

    Update GrokApi.js

--- Selene_v1/app/screens/home-screen/GrokApi.js
+++ Selene_v1/app/screens/home-screen/GrokApi.js
@@ -2,7 +2,7 @@ import Groq from "groq-sdk";
 
 
 // Replace 'your-api-key-here' with your actual API key
-const groq = new Groq({ apiKey: '',
+const groq = new Groq({ apiKey: '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',
   dangerouslyAllowBrowser: true, // Allow the Groq client to be used in the browser
  });
 
